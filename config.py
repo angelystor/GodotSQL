29aa60db3d0caf36e029d019ff38ea35441362f4
Angelystor <kurayami.tenshi@gmail.com>
2017-10-15 23:47:25 -0400

    Removed line that adds empty manifest to the build process, was making Godot not load the module

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ def can_build(plat):
 def configure(env):
 	if env["platform"] == "android":
 		env.android_add_java_dir("android");
-		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
